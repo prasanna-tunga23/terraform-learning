81f04f79256211e248f68dc38dc7f7f9067a4f12
prasanna-tunga23 <68994689+prasanna-tunga23@users.noreply.github.com>
2021-06-18 16:42:51 +0530

    Update vpc.tf

--- demo5/vpc.tf
+++ demo5/vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "pras11-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["us-east-1b"]
+  private_subnets = ["10.0.2.0/24"]
+  public_subnets  = ["10.0.1.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
