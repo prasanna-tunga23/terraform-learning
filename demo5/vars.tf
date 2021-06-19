3c8d9c13a214b4f2ce062d9b7687d55327db56fd
prasanna-tunga23 <68994689+prasanna-tunga23@users.noreply.github.com>
2021-06-19 10:57:57 +0530

    Update vars.tf

--- demo5/vars.tf
+++ demo5/vars.tf
@@ -10,8 +10,8 @@ variable "PATH_TO_PUBLIC_KEY"{
 variable "AMIS"{
   type=map(string)
   default={
-    us-east-1="ami-13be557e"
-    us-west-2="ami-06b94666"
+    #us-east-1="ami-13be557e"
+   # us-west-2="ami-06b94666"
     eu-west-1="ami-844e0bf7"
   }
 }
